84a997ebd727561096c231e9f05e018527a2e7e9
allecatt <157419169+kaikukuu@users.noreply.github.com>
2026-02-19 15:49:47 -0500

    Updated Squirrel.js with comments on to do and working on renderSquirrel()

--- task4/js/Squirrel.js
+++ task4/js/Squirrel.js
@@ -6,11 +6,17 @@ class Squirrel {
         this.color = color;
     }
 
+    //Create a renderSquirrel() method -> which essentially creates a HTML element(s) - could be
+    // an image element:) or an svg .... representing a Squirrel... (see Sun or Flower for inspiration)
     renderSquirrel() {
-
+        this.squirrelDiv = document.createElement('div');
+        this.squirrelDiv.classList.add("squirrel");
+        document.querySelector(".garden").appendChild(this.squirrelDiv);
     }
 
     animateSquirrel() {
 
     }
+
+    //Implement a counter to keep track of how many nuts any given squirrel has picked up (SEE TEAM D for collab)
 }
