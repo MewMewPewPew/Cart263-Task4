094684b5a74c12eca1083f12ec589d7898716b37
allecatt <157419169+kaikukuu@users.noreply.github.com>
2026-02-19 15:37:59 -0500

    Added constructor and methods to squirrel class

---
+++ task4/js/Squirrel.js
@@ -0,0 +1,16 @@
+class Squirrel {
+    //constructor which takes a position, size and color as parameters
+    constructor(position, size, color) {
+        this.position = position;
+        this.size = size;
+        this.color = color;
+    }
+
+    renderSquirrel() {
+
+    }
+
+    animateSquirrel() {
+
+    }
+}
