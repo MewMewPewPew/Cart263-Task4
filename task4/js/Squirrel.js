c9ca7a4505d3b6d98f6fbe92056ff010dfe01c87
allecatt <157419169+kaikukuu@users.noreply.github.com>
2026-02-19 16:12:27 -0500

    Added nutCounter method for future reference

--- task4/js/Squirrel.js
+++ task4/js/Squirrel.js
@@ -19,10 +19,10 @@ class Squirrel {
         this.position = position;
         this.size = size;
         this.color = color;
-    }
+    };
 
     //Create a renderSquirrel() method -> which essentially creates a HTML element(s) - could be an image element:) or an svg .... representing a Squirrel... (see Sun or Flower for inspiration)
-
+//Will be manipulating an SVG element to represent the squirrel. The position, size and color of the squirrel will be set based on the parameters passed to the constructor.
     renderSquirrel() {
         // Create a div element to represent the squirrel and add it to the DOM
         this.squirrelDiv = document.createElement('div');
@@ -37,7 +37,8 @@ class Squirrel {
         // Set the background color of the squirrel div based on the color parameter passed to the constructor
         this.squirrelDiv.style.backgroundColor = this.color;
         //
-    }
+    };
+    
     //Create an animateSquirrel() method in the Squirrel class - which will make a given Squirrel move around the garden - use the requestAnimationFrame()
     animateSquirrel() {
         // Define a function to update the position of the squirrel
@@ -48,7 +49,14 @@ class Squirrel {
             // Request the next animation frame to continue the animation
             requestAnimationFrame(updatePosition);
         };
-    }
+        updatePosition();
+    };
 
     //Implement a counter to keep track of how many nuts any given squirrel has picked up (SEE TEAM D for collab)
+    // Create a method called nutCounter() that increments the nut count and logs it to the console
+    nutCounter() {
+        this.nutCount = this.nutCount + 1;
+        console.log(this.nutCount);
+    }
+
 }
