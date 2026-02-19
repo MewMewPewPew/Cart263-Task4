96c3e2e184e2be97ae0f0f69f14a9e8cce47d327
allecatt <157419169+kaikukuu@users.noreply.github.com>
2026-02-19 15:59:28 -0500

    Updated Squirrel.js Working on outline for working with svg element

--- task4/js/Squirrel.js
+++ task4/js/Squirrel.js
@@ -1,3 +1,18 @@
+//The SVG namespace is required to create SVG elements in JavaScript. It is a string that represents the XML namespace for SVG elements          
+const svgNS = "http://www.w3.org/2000/svg";
+
+// selected element is stored in the variable "garden" for later use when appending the squirrel elements to it.
+const garden = document.querySelector(".garden");
+
+const svg = document.createElementNS(svgNS, "svg");
+svg.setAttribute("width", "100%");
+svg.setAttribute("height", "100%");
+garden.appendChild(svg);
+svg.style.position = "absolute";
+svg.style.top = "0";
+svg.style.left = "0";
+
+
 class Squirrel {
     //constructor which takes a position, size and color as parameters
     constructor(position, size, color) {
@@ -8,6 +23,7 @@ class Squirrel {
 
     //Create a renderSquirrel() method -> which essentially creates a HTML element(s) - could be
     // an image element:) or an svg .... representing a Squirrel... (see Sun or Flower for inspiration)
+
     renderSquirrel() {
         this.squirrelDiv = document.createElement('div');
         this.squirrelDiv.classList.add("squirrel");
